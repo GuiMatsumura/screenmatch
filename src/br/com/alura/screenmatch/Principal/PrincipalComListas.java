6e096082e1bb3fe35353cb81a014883f2ef6bf30
Guilherme Matsumura <guilhermepenamatsumura@gmail.com>
2025-09-17 16:21:50 -0300

    Criando comparação por ano de lançamento do titulo

--- src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -43,6 +44,10 @@ public class PrincipalComListas {
         System.out.println(buscaPorArtista);
         System.out.println("Lista ordenada");
         Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Por ano");
         System.out.println(lista);
     }
 }
