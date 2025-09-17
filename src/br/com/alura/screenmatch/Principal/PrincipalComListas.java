dd864dca3051c29ece3cff6b519f1c611d6c01ec
Guilherme Matsumura <guilhermepenamatsumura@gmail.com>
2025-09-17 16:30:41 -0300

    Adicionando o conceito de List e LinkedList

--- src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
@@ -4,9 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -18,7 +16,7 @@ public class PrincipalComListas {
         Filme meuFilme = new Filme("O poderoso chefão", 1970);
         meuFilme.avalia(10);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
         lista.add(filmeDoPaulo);
         lista.add(meuFilme);
         lista.add(outroFilme);
