eadf3a50a3ad9dc17463b8e164fa2241a35e0acb
Guilherme Matsumura <guilhermepenamatsumura@gmail.com>
2025-09-18 11:32:16 -0300

    Criando argumento SerializedName para busca efetiva e lidando com exceções

--- src/br/com/alura/screenmatch/Principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/Principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.Principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -13,15 +19,38 @@ public class PrincipalComBusca {
         System.out.println("Digite o nome de um filme");
         String busca = leitura.nextLine();
 
-        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=***";
+        String endereco = "http://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=****";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+
+            TituloOmdb meuTituloOmdb = gson.fromJson(response.body(), TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+
+            //try {
+            System.out.println("Titulo convertido");
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao tentar converter o filme");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro ao tentar buscar o filme");
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Programa finalizado");
     }
 }
